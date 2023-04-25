3865615a1fcf703322e886559908226eaaf8a911
Matin Mt <matinmt2004@gmail.com>
2023-04-25 19:15:39 +0430

    first commit - completed Movie.java

---
+++ Movie.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Movie {
+    private int id;
+    private int counter = 1;
+    private String title;
+    private String genre;
+    private String director;
+    private ArrayList<String> cast;
+    private Date releaseDate;
+    private boolean isAvailable;
+    private double fee;
+
+    public Movie(String title, String genre, String director, ArrayList<String> cast,
+                 Date releaseDate, double Fee) {
+        this.id = counter++;
+        this.title = title;
+        this.genre = genre;
+        this.director = director;
+        this.cast = new ArrayList<>(cast);
+        this.releaseDate = releaseDate;
+        this.fee = Fee;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public ArrayList<String> getCast() {
+        return cast;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable (boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+}
+
