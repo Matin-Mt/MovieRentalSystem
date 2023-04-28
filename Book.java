b09db9ebc2cf3fda4488e3cb0a0041e031d39ec5
Matin Mt <matinmt2004@gmail.com>
2023-04-28 12:24:15 +0430

    book class completed - book extends item

--- Book.java
+++ Book.java
@@ -1,2 +1,26 @@
-package org.example;public class Book {
+package org.example;
+
+import java.util.Date;
+
+public class Book extends Item {
+    private String author;
+    private String genre;
+
+    public Book(String title, String author, String genre, Date releaseDate, double fee) {
+        super(title, releaseDate, fee);
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public Book(String title, String author, String genre, Date releaseDate) {
+        this(title, author, genre, releaseDate, 0);
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
 }
